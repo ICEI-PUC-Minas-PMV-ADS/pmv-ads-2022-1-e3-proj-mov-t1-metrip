aab1d1694de6133127b427bc404fea0dc11785c5
RodrigoLacerdaa <81272703+RodrigoLacerdaa@users.noreply.github.com>
2022-06-22 20:37:00 -0300

    Update App.js

--- src/metrip_v1/App.js
+++ src/metrip_v1/App.js
@@ -1,29 +1,9 @@
 import React from 'react';
-import {View, Text, StyleSheet, Constants} from 'react-native';
 
-import Login from './src/pages/login'
-import img from './src/pages/imagem-padrao.jpg'
+import Home from './src/pages/Home';
 
-const App= () => {
-
-  return(
-    <Login> </Login>
-  );
-}
+const App = () => {
+  return <Home/>;
+};
 
 export default App;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    backgroundColor: '#ecf0f1',
-    padding: 8,
-  },
-  paragraph: {
-    margin: 24,
-    fontSize: 18,
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
-});
